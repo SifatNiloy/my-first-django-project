b5f3e525762149efaea91c20f5e755234ffbcf8a
unknown <sifatniloy18@gmail.com>
2022-12-17 00:35:06 -0600

    sent dynamic data with context dictionary

--- mysite/views.py
+++ mysite/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    name="patrick"
-    return render(request, "index.html",{'name': name})
+    context={
+        'name': 'sifat niloy',
+        'age': 25, 
+        'nationality':'Bangladeshi'
+    }
+    return render(request, "index.html",context)
