47c9157b3c197498a7affc3e0807122c3c5686fc
unknown <sifatniloy18@gmail.com>
2022-12-17 01:00:50 -0600

    multiple url added and get operation form input textarea

--- mysite/views.py
+++ mysite/views.py
@@ -8,3 +8,8 @@ def index(request):
         'nationality':'Bangladeshi'
     }
     return render(request, "index.html",context)
+
+def counter(request):
+    text= request.GET['text']
+    amount_of_words=len(text.split())
+    return render(request,'counter.html', {'amount':amount_of_words})
